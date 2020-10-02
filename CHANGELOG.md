7d5936d614c89b37f92f1b4ae2e9ed3cbc3cad8e
SL8R <islaterm@gmail.com>
2020-10-02 16:48:14 -0300

    Test for mismatching size in chromosome
    Took 6 minutes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ## Version 1.0
 
+- (B.9) Test for mismatching size in chromosome
 - (B.8) Test for empty chromosome
 - (B.7) Implemented chromosome class and mutation method
 - (B.6) Test for ASCII gene
--- genetics/test/test_chromosomes.py
+++ genetics/test/test_chromosomes.py
@@ -4,12 +4,19 @@ from typing import Dict
 
 import pytest
 
-from genetics.chromosomes import GenericChromosome
+from genetics.chromosomes import GenericChromosome, GenotypeException
+from genetics.genes import GenericGene
 
 
-def test_empty_chromosome(binary_alphabet: Dict):
-    test_chromosome = GenericChromosome(0, binary_alphabet, genes=[])
-    assert not len(test_chromosome)
+def test_empty_chromosome(binary_alphabet: Dict) -> None:
+    generic_chromosome = GenericChromosome(0, binary_alphabet, genes=[])
+    assert not len(generic_chromosome)
+
+
+def test_size_mismatch_chromosome(binary_alphabet: Dict, size_mismatch_error_msg: str) -> None:
+    with pytest.raises(GenotypeException) as genotype_error:
+        _ = GenericChromosome(0, binary_alphabet, genes=[GenericGene(binary_alphabet)])
+        assert size_mismatch_error_msg in genotype_error.value
 
 
 @pytest.fixture()
@@ -20,6 +27,13 @@ def binary_alphabet() -> Dict[bool, int]:
 @pytest.fixture()
 def ascii_alphabet() -> Dict[int, str]:
     return dict([n for n in enumerate(string.ascii_letters)])
+
+
+@pytest.fixture()
+def size_mismatch_error_msg() -> str:
+    return "Chromosome size doesn't match the number of genes.\n" \
+           "Expected: 0\n" \
+           "Actual: 1"
 
 
 if __name__ == '__main__':
